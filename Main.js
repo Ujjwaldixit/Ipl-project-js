509fcf248d79e13d18dbc3ad98d53c26c5eb6859
Ujjwal Dixit <ujjwal.dixit.17.1@mountblue.tech>
2021-09-12 19:03:34 +0530

    feat(total matches per city): add method to calculate total matches per city

--- Main.js
+++ Main.js
@@ -23,3 +23,24 @@ setTimeout(function(){
 },1000);
 
 
+function findTotalMatchesPlayedPerCity(matches)
+{
+    var totalMatchesPlayedPerCity={};
+    console.log(matches.length);   
+    for(var i=0;i<matches.length;i++)
+    {
+        let city=matches[i].city;
+        if(totalMatchesPlayedPerCity.hasOwnProperty(city))
+        {
+           // console.log("inside if");
+            totalMatchesPlayedPerCity[city]=totalMatchesPlayedPerCity[city]+1;
+        }
+        else{
+           // console.log("inside else");
+            totalMatchesPlayedPerCity[city]=1;
+        }
+    }
+    console.log(totalMatchesPlayedPerCity);
+}
+
+
